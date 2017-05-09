f863b0d1077337c044dea5175f26fcc9603bd4c7
Eric <eric@someone.com>
2017-05-08 17:59:54 -0700

    Basic constructors done

---
+++ ClozeCard.js
@@ -0,0 +1,37 @@
+var BasicCard = require("./BasicCard.js");
+
+function ClozeCard(text, cloze) {
+    if (this instanceof ClozeCard) {
+        this.cloze = cloze;
+        this.partial = text.replace(cloze, '***');
+        this.fullText = text;
+    } else {
+        return new ClozeCard(text, cloze);
+    }
+}
+
+module.exports = ClozeCard;
+
+//Testing
+
+var firstPresident = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+
+console.log(firstPresident.front);
+
+console.log(firstPresident.back);
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(firstPresidentCloze.cloze);
+
+// " ... was the first president of the United States.
+console.log(firstPresidentCloze.partial);
+
+// "George Washington was the first president of the United States.
+console.log(firstPresidentCloze.fullText);
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
