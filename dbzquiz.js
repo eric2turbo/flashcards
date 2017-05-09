a46ebf863b1d74fd328edb667313f226f0f4b60f
Eric <eric@someone.com>
2017-05-08 20:26:33 -0700

    added more questions

--- dbzquiz.js
+++ dbzquiz.js
@@ -17,7 +17,20 @@ var dbz4 = new ClozeCard(
 var dbz5 = new ClozeCard(
     "Cell turned into his perfect form by absorbing Androids 17 and 18", "Cell");
 
-var questions = [dbz1, dbz2, dbz3, dbz4, dbz5];
+var dbz6 = new ClozeCard(
+    "The Prince of Saiyans is Vegeta", "Vegeta");
+
+var dbz7 = new ClozeCard(
+    "Buu has the power to turn people into candy", "Buu");
+
+var dbz8 = new ClozeCard(
+    "Yamcha is also a professional baseball player", "Yamcha");
+
+var dbz9 = new ClozeCard(
+    "Goku's technique where he shoots energy from his hands is called the Kamehameha", "Kamehameha"
+);
+
+var questions = [dbz1, dbz2, dbz3, dbz4, dbz5, dbz6, dbz7, dbz8, dbz9];
 
 var i = 0;
 var corrects = 0;
@@ -30,13 +43,13 @@ function quiz() {
     }, ]).then(function(user) {
         if (user.answer.toLowerCase() === questions[i].cloze.toLowerCase()) {
             console.log("Correct! The answer was: " + questions[i].cloze);
-            console.log("===========================================================");
+
             corrects++;
         } else {
             console.log("Wrong! The correct answer was: " + questions[i].cloze);
-            console.log("===========================================================")
             incorrects++;
         }
+        console.log("================================================================");
         i++;
         if (i === questions.length) {
             console.log("Results: You got " + corrects + " right and " + incorrects + " wrong.");
